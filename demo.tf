e63f920d9627e8b45415cc58ed59154e2a23ff40
sreemanos <74346692+sreemanos@users.noreply.github.com>
2023-09-04 17:54:31 +0530

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -1,41 +1,20 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+locals {
+  arr = ["host1", "host2", "host3"]
 }
 
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIAX53T7J3SO7EGZ7MQ"
-  secret_key = "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"
+
+locals {
+  arr2 = ["host1", "host2", "host3"]
 }
 
-# resource "aws_instance" "ec2" {
-#     ami = "ami-08a52ddb321b32a8c"
-#     instance_type = "t2.micro" 
-#     count = 3
-#   } 
-
-variable "names" {
-    type = list
-    default = ["dev","stage","prod"]
+locals {
+  arr3 = ["host1", "host2", "host3"]
 }
 
-resource "aws_iam_user" "lb" {
-  name = var.names[count.index]
-  count = 3
-  path = "/system/"
-}  
+output "test" {
+  value = local.arr
+}
 
-# resource "aws_instance" "ec22" {
-#     ami = "ami-08a52ddb321b32a8c"
-#     instance_type = "t2.micro"
-#   } 
-
-# resource "aws_instance" "ec21" {
-#     ami = "ami-08a52ddb321b32a8c"
-#     instance_type = "t2.micro"
-#   } 
+output "tes2" {
+  value = local.arr3
+}
